d7ffd2437194b190276038c8fcf6fefa75836d1e
Hitesh Gohil <hitesh@abnd.in>
2022-06-22 18:37:30 +0530

    22.6.22 Day End

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Header.css";
 import { Link } from "react-router-dom";
-import { FaBeer } from "react-icons/fa";
+import { HiMenuAlt3 } from "react-icons/hi";
+import { IoClose } from "react-icons/io5";
+import { motion } from "framer-motion";
 
 function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => setIsOpen(!isOpen);
+
   return (
     <div className="header">
       <div className="container">
@@ -12,16 +17,27 @@ function Header() {
             <img
               src="https://abnd.in/wp-content/uploads/2019/11/Logo.png"
               alt="Logo"
+              className="logo"
             />
           </Link>
         </div>
-        <div className="nav">
+        <motion.div
+          animate={{
+            transform: isOpen ? "translatex(0%)" : "translatex(100%)",
+          }}
+          className="nav"
+        >
           <Link to="/">Home</Link>
           <Link to="/about">About</Link>
           <Link to="/services">Services</Link>
           <Link to="/work">Work</Link>
           <Link to="/contact">Contact</Link>
-          <FaBeer />
+        </motion.div>
+        <div className="menu-icon">
+          <IoClose className="icon-close" />
+          <motion.div animate={{ opacity: isOpen ? "0" : "1" }}>
+            <HiMenuAlt3 className="icon-hamburger" onClick={toggle} />
+          </motion.div>
         </div>
       </div>
     </div>
