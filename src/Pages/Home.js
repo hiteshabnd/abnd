b4a7d469dc133693652a7eef61d4fc85979c6cbf
Hitesh Gohil <hitesh@abnd.in>
2022-07-05 14:12:58 +0530

    22.7.05 CSS Grids added in the Files

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -6,7 +6,87 @@ function Home() {
   return (
     <PageAnimation>
       <Banner>
-        <h1>Home</h1>
+        <div className="flex flex8">
+          <div>1</div>
+          <div>2</div>
+          <div>3</div>
+          <div>4</div>
+          <div>5</div>
+          <div>6</div>
+          <div>7</div>
+          <div>8</div>
+        </div>
+      </Banner>
+      <Banner>
+        <div className="flex flex6">
+          <div>1</div>
+          <div>2</div>
+          <div>3</div>
+          <div>4</div>
+          <div>5</div>
+          <div>6</div>
+        </div>
+      </Banner>
+      <Banner>
+        <div className="flex flex5">
+          <div>1</div>
+          <div>2</div>
+          <div>3</div>
+          <div>4</div>
+          <div>5</div>
+        </div>
+      </Banner>
+      <Banner>
+        <div className="grid grid8">
+          <div>1</div>
+          <div>2</div>
+          <div>3</div>
+          <div>4</div>
+          <div>5</div>
+          <div>6</div>
+          <div>7</div>
+          <div>8</div>
+        </div>
+      </Banner>
+      <Banner>
+        <div className="grid grid6">
+          <div>1</div>
+          <div>2</div>
+          <div>3</div>
+          <div>4</div>
+          <div>5</div>
+          <div>6</div>
+        </div>
+      </Banner>
+      <Banner>
+        <div className="grid grid5">
+          <div>1</div>
+          <div>2</div>
+          <div>3</div>
+          <div>4</div>
+          <div>5</div>
+        </div>
+      </Banner>
+      <Banner>
+        <div className="grid grid4">
+          <div>1</div>
+          <div>2</div>
+          <div>3</div>
+          <div>4</div>
+        </div>
+      </Banner>
+      <Banner>
+        <div className="grid grid3">
+          <div>1</div>
+          <div>2</div>
+          <div>3</div>
+        </div>
+      </Banner>
+      <Banner>
+        <div className="grid grid2">
+          <div>1</div>
+          <div>2</div>
+        </div>
       </Banner>
     </PageAnimation>
   );
